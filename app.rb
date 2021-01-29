25e61544cbdbf7c065e78c972807a8bf8d65166d
Serg <seregaproger@yandex.ru>
2021-01-29 22:29:04 +0300

    add form for new Movie, created several Movies in app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require './lib/movie'
 
 get('/movies') do  
-	@movie = Movie.new
-	@movie.title = "Jaws"
+	@movies = []
+    @movies[0] = Movie.new
+    @movies[0].title = "Jaws"
+    @movies[1] = Movie.new
+    @movies[1].title = "Alen"
+    @movies[2] = Movie.new
+    @movies[2].title = "Term"
 	erb :index
+end
+
+get('/movies/new') do
+  erb :new
 end
