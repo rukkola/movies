c10ab3e3570ebadc718ee3897487e9c07e6e7234
Serg <seregaproger@yandex.ru>
2021-01-30 17:51:03 +0300

    save film, update list all films

--- app.rb
+++ app.rb
@@ -1,17 +1,27 @@
 require 'sinatra'
 require './lib/movie'
+require './lib/movie_store'
+
+store = MovieStore.new('movies.yml')
 
 get('/movies') do  
-	@movies = []
-    @movies[0] = Movie.new
-    @movies[0].title = "Jaws"
-    @movies[1] = Movie.new
-    @movies[1].title = "Alen"
-    @movies[2] = Movie.new
-    @movies[2].title = "Term"
+	#@movies = []
+    #@movies[0] = Movie.new
+    #@movies[0].title = "Jaws"
+    @movies = store.all
 	erb :index
 end
 
 get('/movies/new') do
   erb :new
+end
+
+post('/movies/create') do  
+	@movie = Movie.new
+	@movie.title = params['title']
+	@movie.director = params['director']
+	@movie.year = params['year']
+	store.save(@movie)
+	redirect '/movies/new'
+	"Received: #{params.inspect}"
 end
